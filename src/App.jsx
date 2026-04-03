e84b8e3d8d79ede9ef99a65dbe5be5c7f678b43b
emorgan <erinannmorgan@yahoo.com>
2026-04-03 10:34:12 -0500

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { Analytics } from '@vercel/analytics/react'
+import SubSheetOptimizer from './SubSheetOptimizer'
+
+export default function App() {
+  return (
+    <>
+      <SubSheetOptimizer />
+      <Analytics />
+    </>
+  )
+}
